d16770dfde652acbd6ee206519828f7b7f27911a
Ramah Hashem Madi <ramahmadi2001@gmail.com>
2023-10-01 18:17:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+function saturdayFun(string = "roller-skate"){
+    return `This Saturday, I want to ${string}!`
+}
+
+function mondayWork(string = "go to the office"){
+    return `This Monday, I will ${string}.`
+}
+
+function wrapAdjective(symbol){
+    return function (string){
+      return `You are ${symbol}${string}${symbol}!`
+    }
+}
+
